6faf186fae0524db7a9f8b7e4d2901177c1a968a
Arun Kumar <arun111143@gmail.com>
2023-12-17 16:49:36 +0530

    first commit

---
+++ src/main/java/com/johndeer/johnDeer/JohnDeerApplication.java
@@ -0,0 +1,13 @@
+package com.johndeer.johnDeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnDeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnDeerApplication.class, args);
+	}
+
+}
